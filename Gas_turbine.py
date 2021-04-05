7f1e0c5fe12b010fdfb17666819dd5282ac13c60
a123verkho <alexander.verkholomov@gmail.com>
2021-04-05 22:47:10 +0300

    fisrt commit

---
+++ Gas_turbine.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 21:01:33 2021
+
+@author: AVERKHO
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.close('all')
+
+import os
+
+files=os.listdir('./')
+print(files)
