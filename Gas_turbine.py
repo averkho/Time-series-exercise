ca0a360498f7071d502e01041f3d7a6e43338258
a123verkho <alexander.verkholomov@gmail.com>
2021-04-07 13:13:38 +0300

    the third commit

--- Gas_turbine.py
+++ Gas_turbine.py
@@ -9,7 +9,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.close('all')
 
-import os
 
-files=os.listdir('./')
-print(files)
+dat=pd.read_excel('./Gas Turbine.xlsx')
+
+fig=plt.figure(1)
+ax=fig.add_subplot(111)
+line1=ax.plot(dat['Output'],label='Output',color='blue')
+ax.set_ylabel('Output, MW')
+ylims=ax.get_ylim()
+ax.set_ylim([25,ylims[1]])
+
+ax2=ax.twinx()
+line2=ax2.plot(dat['Temperature'],label='Temperature',color='red')
+ax.set_xticklabels([])
+ax2.set_ylabel('Temperature, 0C')
+ax.grid(True)
+
+lines=line1+line2
+labels=[l.get_label() for l in lines]
+plt.legend(lines,labels,loc='lower left')
+ax.set_title('Gas Turbine productivity',size=14)
